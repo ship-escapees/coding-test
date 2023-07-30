e8b359120c451a8f4378a816644f3f7cea6ab38f
ingwang <dlsrhkd615@gmail.com>
2023-07-30 13:22:15 +0900

    add: 프로그래머스 2레벨 게임 맵 최단거리 문제 풀이

---
+++ ingwang/programmers-2/게임-맵-최단거리.java
@@ -0,0 +1,54 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+
+   public static int solution(int[][] maps) {
+
+        int[] dx = {0,1,-1,0};
+        int[] dy = {1,0,0,-1};
+        int answer = 0;
+
+        int[][] visited = new int[maps.length][maps[0].length];
+
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{0,0}); // 시작점
+        visited[0][0] = 1;
+
+        while(!q.isEmpty()){
+            int[] m = q.poll();
+            int x = m[0];
+            int y = m[1];
+            
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if(nx <0 || nx > maps.length -1 || ny <0 || ny > maps[0].length-1){
+                    continue;
+                }
+
+                if (visited[nx][ny]==0 && maps[nx][ny]==1){
+                    visited[nx][ny] = visited[x][y]+1;
+                    q.add(new int[]{nx,ny});
+                }
+
+            }
+        }
+
+        answer = visited[maps.length-1][maps[0].length-1];
+
+        if(answer==0){
+            answer = -1;
+        }
+
+        return answer;
+
+  }
+
+        
+    public static void main(String[] args) {
+        int [][] x = {{1,0,1,1,1},{1,0,1,0,1},{1,0,1,1,1},{1,1,1,0,1},{0,0,0,0,1}}; 
+        System.out.println(solution(x));
+    }
+}   
