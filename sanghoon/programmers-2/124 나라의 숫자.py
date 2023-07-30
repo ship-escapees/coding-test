896757d94bb534aa6f94fa8f7913a1de4d62dbc4
23hoon <nicet.shlee@gmail.com>
2023-07-30 20:02:34 +0900

    refactor : 레벨2 124 나라의 숫자 리팩토링

--- sanghoon/programmers-2/124 나라의 숫자.py
+++ sanghoon/programmers-2/124 나라의 숫자.py
@@ -1,25 +1,13 @@
 def solution(n):
-    list = [10,20,70]
-    cnt = 1
-    answer = 0
-    index = 0
-    while 3 * cnt < n :
-        cnt = cnt + 1
-        if index == 3:
-            index = 0
-        answer = answer + list[index]
-        index = index + 1
-    answer = answer - list[index-1]
-    print("cnt", cnt -1)
-    if cnt > 1:
-        answer = answer + 10
-    print("answer", answer)
-
-    last = n%3
-    if last == 0 :
-        last = 4
-    answer = answer + last
-    print("n%3", n%3)
-    print("정답",answer)
-n = 30
+    answer = ""
+    list = ['4','1','2']
+    while n :
+        if n % 3 == 0 :
+            answer += list[n%3]
+            n = n//3 - 1
+        else:
+            answer += list[n%3]
+            n = n//3
+    print(answer[::-1])
+n = 3
 solution(n)
