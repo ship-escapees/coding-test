8843720d19c03600ae5b2f72f3ae32619aad0c1b
ingwang <dlsrhkd615@gmail.com>
2023-08-13 18:07:18 +0900

    add: 프로그래머스 레벨2 올바른-괄호 문제 풀이

---
+++ ingwang/programmers-2/올바른-괄호.java
@@ -0,0 +1,16 @@
+class Solution6 {
+   public static boolean solution6(String s) {
+        boolean answer =true;
+
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i++) {
+        
+        }
+        return answer;
+  }
+
+        
+    public static void main(String[] args) {
+        System.out.println(solution6("board"));
+    }
+}   
