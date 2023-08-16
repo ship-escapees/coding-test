88f3d506fad4a7463c736bf5707996db016f5b9d
ikkim <ikkim@ncurity.com>
2023-08-16 16:19:13 +0900

    add: 8월 2주차 문제 풀이

--- ingwang/programmers-2/올바른-괄호.java
+++ ingwang/programmers-2/올바른-괄호.java
@@ -1,10 +1,23 @@
 class Solution6 {
    public static boolean solution6(String s) {
         boolean answer =true;
+        int num[] = new int[s.length()];
+        int result = 0;
 
         char[] charArray = s.toCharArray();
         for (int i = 0; i < charArray.length; i++) {
-        
+            if(charArray[i]=='('){
+                num[i]=1;
+            }else{
+                num[i]=-1;
+            }
+            result += num[i];
+            if (result<0){
+                return false;
+            }
+        }
+         if(result != 0){
+            return false;
         }
         return answer;
   }
