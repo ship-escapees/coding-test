bc306d28f08d636948268ee0088d3261257cf239
kyuung09 <kyuung09@gmail.com>
2023-09-02 19:06:05 +0900

    add : 프로그래머스 레벨2 줄서는 방법

---
+++ kyulee/programmers-2/줄-서는-방법.java
@@ -0,0 +1,25 @@
+
+public class 줄 서는 방법 {
+    public int[] solution(int n, long k) {
+        int[] answer = new int[n];
+        List<Integer> nList = new ArrayList<>();
+        long factorial = 1;
+
+        for (int i = 1; i <= n; i++) {
+            nList.add(i);
+            factorial *= i;
+        }
+
+        k--;
+
+        for (int i = 0; i < n; i++) {
+            factorial /= (n - i);
+            int idx = (int) (k / factorial);
+            k %= factorial;
+
+            answer[i] = nList.get(idx);
+            nList.remove(idx);
+        }
+        return answer;
+    }
+}
