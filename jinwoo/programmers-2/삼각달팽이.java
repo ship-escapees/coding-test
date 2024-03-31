296d49ec311e2830b1fa0e2c27d35f44a44a2886
woooo96 <chsami3@naver.com>
2024-03-31 22:59:43 +0900

    add 프로그래머스 2레벨 삼각달팽이, 쿼드압축 후 개수 세기 문제풀이

---
+++ jinwoo/programmers-2/삼각달팽이.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public int[] solution(int n) {
+        int[][] snail = new int[n][n];
+        int x = -1, y = 0;
+        int num = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (i % 3 == 0) {
+                    x++;
+                } else if (i % 3 == 1) {
+                    y++;
+                } else {
+                    x--;
+                    y--;
+                }
+                snail[x][y] = num++;
+            }
+        }
+
+        int[] answer = new int[n * (n + 1) / 2];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                answer[index++] = snail[i][j];
+            }
+        }
+
+        return answer;
+    }
+}
